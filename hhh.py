dab08a1888be50ef53a12324ed63abb6a5eb9a50
Fei Hua <feihua@Feis-MacBook-Pro.local>
2021-02-13 14:59:45 -0500

    update

--- hhh.py
+++ hhh.py
@@ -6,8 +6,11 @@ with open(src, 'r') as f:
    	lines = f.readlines()
 
 with open(tar, 'w') as f:
-	for line in lines[2:]:
+	for line in lines[1:]:
 		line = line.rstrip('\n')
+		if line.startswith('qreg '):
+			newline = 'qbit ' + line[5:-1] + ';\n'
+			f.write(newline)
 		if line.startswith('h '):
 			newline = 'H (' + line[2:-1] + ');\n'
 			f.write(newline)
